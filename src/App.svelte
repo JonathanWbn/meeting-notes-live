9f1bc5660bb558c293f263f68105df2137051f2b
JonathanWbn <jwieben@hey.com>
2020-12-26 21:31:59 +0100

    New: Build UI

---
+++ src/App.svelte
@@ -0,0 +1,85 @@
+<script>
+	import GlobalStyles from "./GlobalStyles.svelte";
+	import InfoIcon from "./InfoIcon.svelte";
+</script>
+
+<style>
+	.wrapper {
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-top: 20px;
+		width: 100%;
+		max-width: 700px;
+		padding: 0 20px;
+	}
+
+	h2 {
+		margin: 0;
+	}
+
+	.info {
+		color: rgb(100, 100, 100);
+		transition-property: color 150ms cubic-bezier(0.4, 0, 0.2, 1);
+		display: flex;
+		text-decoration: none;
+		align-items: center;
+	}
+
+	.info:hover {
+		color: black;
+	}
+
+	.info :global(svg) {
+		margin-right: 5px;
+	}
+
+	main {
+		flex-grow: 1;
+		padding: 20px;
+		width: 100%;
+		max-width: 700px;
+	}
+
+	textarea {
+		width: 100%;
+		height: 100%;
+		padding: 15px;
+		border: none;
+		border-radius: 10px;
+		font-size: 20px;
+		box-shadow: 0 0 3rem rgba(128, 128, 128, 0.411);
+		outline: none;
+		font-family: "Inconsolata", monospace;
+	}
+
+	textarea::-webkit-resizer {
+		display: none;
+	}
+</style>
+
+<GlobalStyles />
+<div class="wrapper">
+	<header>
+		<h2>Meeting Notes Live</h2>
+		<a
+			target="_blank"
+			class="info"
+			href="https://www.notion.so/jwieben/Meeting-Notes-Live-dd9571901e4e4b6bbc96225005850e8a">
+			<InfoIcon />
+			How does this work?
+		</a>
+	</header>
+	<main>
+		<!-- svelte-ignore a11y-autofocus -->
+		<textarea autofocus placeholder="Type notes here..." />
+	</main>
+</div>
---
+++ src/GlobalStyles.svelte
@@ -0,0 +1,19 @@
+<style>
+  :global(*) {
+    box-sizing: border-box;
+    font-family: "Inter", sans-serif;
+  }
+
+  :global(body) {
+    margin: 0;
+  }
+</style>
+
+<svelte:body />
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Inter&display=swap"
+    rel="stylesheet" />
+</svelte:head>
