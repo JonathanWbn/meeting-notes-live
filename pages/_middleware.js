4f6520011cfa965b5002d547f25e4368d6aa6082
JonathanWbn <jwieben@hey.com>
2021-11-28 10:51:28 +0100

    refactor: use edge functions

---
+++ pages/_middleware.js
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server'
+import randomstring from 'randomstring'
+
+import { set } from '../lib/redis'
+
+export async function middleware(req) {
+  if (req.nextUrl.pathname === '/') {
+    const id = randomstring.generate(7)
+    await set(id, '')
+
+    const response = NextResponse.redirect(`/${id}`)
+
+    response.headers.set(
+      'Set-Cookie',
+      `is-author-${id}=true; Expires=Wed, 21 Oct 3000 07:28:00 GMT; Path=/`
+    )
+
+    return response
+  }
+}
